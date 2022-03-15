0e777208edf1c3cfdac818ddcfd9c3f3efce2c20
Phil Jay <phil.jay@reece.com.au>
2022-03-16 10:48:19 +1100

    No timestamp

--- scripts/blat.sh
+++ scripts/blat.sh
@@ -6,9 +6,9 @@
 
 set -x
 
-rsync ${3} --exclude '*.sw?' -HPvax --no-o --no-g --no-p generic/ ~/
-rsync ${3} --exclude '*.sw?' -HPvax --no-o --no-g --no-p ${2}/ ~/
-rsync ${3} --exclude '*.sw?' -HPvax --no-o --no-g --no-p locals/${1}/ ~/
+rsync ${3} --exclude '*.sw?' -HPvax --no-t --no-o --no-g --no-p generic/ ~/
+rsync ${3} --exclude '*.sw?' -HPvax --no-t --no-o --no-g --no-p ${2}/ ~/
+rsync ${3} --exclude '*.sw?' -HPvax --no-t --no-o --no-g --no-p locals/${1}/ ~/
 
 DATE=`git show -s --format=%ci -q HEAD`
 COMMIT=`git rev-parse --short HEAD`
